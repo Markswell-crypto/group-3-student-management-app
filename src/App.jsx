3041b66c35e317e3cc17dab62b2471d5018782ad
Markswell <osoromarkswell80@gmail.com>
2023-11-02 11:36:05 +0300

    Creating PATCH

--- src/App.jsx
+++ src/App.jsx
@@ -1,72 +1,54 @@
-import { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css'
 import Students from './components/Students'
 
 function App() {
-   const BASE_URL ='http://localhost:3000/studentsData'
+  const BASE_URL = 'http://localhost:3000/studentsData';
 
-  const student ={
-    name:'',
-subject:'',
-marks:'',
-}
-const [data, setData] =useState(student)
-const [users, setUsers]= useState([])
+  const [data, setData] = useState({ name: '', subject: '', marks: '' });
+  const [students, setStudents] = useState([]);
 
+  function handleFormData(e) {
+    setData({ ...data, [e.target.name]: e.target.value });
+  }
 
-function handleFormData (e) {
-  
-  setData ({...data, [e.target.name]:e.target.value})
-}
+  function handleSubmit(e) {
+    e.preventDefault();
+    fetch(BASE_URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+      .then((res) => res.json())
+      .then((stud) => {
+        setStudents([...students, stud]);
+        setData({ name: '', subject: '', marks: '' });
+      });
+  }
 
-function handleSubmit(e){
-  e.preventDefault()
-  console.log('ha')
+  useEffect(() => {
+    fetch(BASE_URL)
+      .then((res) => res.json())
+      .then((data) => setStudents(data));
+  }, []);
 
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="name" value={data.name} placeholder="name" required onChange={handleFormData} />
+        <input type="text" name="subject" value={data.subject} placeholder="subject" required onChange={handleFormData} />
+        <input type="number" name="marks" value={data.marks} placeholder="marks" required onChange={handleFormData} />
+        <br></br>
+        <button type="submit">Submit</button>
+      </form>
 
-
-
-fetch(BASE_URL, {
-
-  method:"POST",
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  body:JSON.stringify (student)
-  
-
-})
-.then((res)=> res.json())
-.then((stud) => setData(...data, stud)  )
-}
-useEffect(()=> {
-  fetch(BASE_URL)
-  .then((res)=> res.json)
-  .then((data)=> setData(data))
-},[])
-
-
-return (
-   
-      <div>
-<form onSubmit={handleSubmit}>
-  < input type='text'  name ='name' value={data['name']} placeholder='name' required onChange={handleFormData} />
-  < input type='text'  name ='subject' value={data['subject']} placeholder='subject' required onChange={handleFormData} />
-  < input type='number' name= 'marks' value={data['marks']} placeholder='marks' required onChange={handleFormData} />
-  <br></br>
-  <button type='submit'>Submit</button>
-
-</form>
-
-
-
-
-
-      <Students/>
-      </div>
-     
-   
-  )
+      {students.map((student, index) => (
+        <Students key={index} id={student.id} students={students} setStudents={setStudents} name={student.name} subject={student.subject} marks={student.marks} setData={setData} data={data}/>
+      ))}
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/components/Students.jsx
+++ src/components/Students.jsx
@@ -1,24 +1,62 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-function Students({name, subject, marks}) {
+function Students({name, subject, marks, id, students, setStudents}) {
+  const [data, setData] = useState({ name:'' , subject:'' , marks:'' });
+
+  function handleUpdate(e) {
     
-
-    const student ={
-        name:'',
-    subject:'',
-    marks:'',
+    setData({ ...data, [e.target.name]: e.target.value });
+  }
+    function handleDelete(){
+      fetch(`http://localhost:3000/studentsData/${id}`,{
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+      .then(res => res.json())
+      .then(() => {
+        let deleteStud = students.filter((stud) => stud.id !== id)
+        setStudents(deleteStud)
+      })
     }
-
-
-
-
-
+    function handleUpdatedForm(){
+      fetch(`http://localhost:3000/studentsData/${id}`,{
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      }) 
+      .then(res => res.json())
+      .then(() => {
+        let updatedStud = data.map(studData => {
+          if(studData.id === id){
+            studData.name = data.name
+            studData.subject = data.subject
+            studData.marks = data.marks
+          }
+          return studData
+        })
+        setData(updatedStud)
+        setData({ name: '', subject: '', marks: '' });
+      })
+    }
   return (
     <div>
-      <h1>Student Data</h1>
-      <p>name:{name}</p>
-      <p>subject:{subject}</p>
-      <p>marks:{marks}</p>
+      <h4>Student Data</h4>
+      <p>name: {name}</p>
+      <p>subject: {subject}</p>
+      <p>marks: {marks}</p>
+      <form onSubmit={handleUpdatedForm}>
+        <input type="text" name="name" value={data.name} placeholder="Update Name" required onChange={handleUpdate} />
+        <input type="text" name="subject" value={data.subject} placeholder="Update Subject" required onChange={handleUpdate} />
+        <input type="number" name="marks" value={data.marks} placeholder="Update Marks" required onChange={handleUpdate} />
+        <br></br>
+        <button type="submit">Update</button>
+      </form>
+        <br></br>
+      <button onClick={handleDelete} >Delete</button>
     </div>
   )
 }
